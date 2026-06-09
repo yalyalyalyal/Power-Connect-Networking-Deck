ba336026e348a1232c19047c5c443d774a079546
Eyal <eyal.ephrati@gmail.com>
2026-06-09 14:21:19 +0300

    Update vite.config.ts
    Added tailwindcss import

--- vite.config.ts
+++ vite.config.ts
@@ -8,10 +8,12 @@ import { defineConfig } from 'vite';
 import viteReact from '@vitejs/plugin-react';
 import { nitro } from 'nitro/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [
     tsconfigPaths(),
+    tailwindcss(),
     tanstackStart(),
     nitro(),
     viteReact(),
