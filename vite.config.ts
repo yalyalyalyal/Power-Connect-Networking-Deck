0abef1eb2bb41aa5f3f05b6417ad2efa48e3a3b6
Eyal <eyal.ephrati@gmail.com>
2026-06-09 13:56:26 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,11 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import { defineConfig } from 'vite';
 import viteReact from '@vitejs/plugin-react';
 import { nitro } from 'nitro/vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [
+    tsconfigPaths(),
     tanstackStart(),
     nitro(),
     viteReact(),
